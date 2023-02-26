11cb8d3946550e20077bc0b7e288fcfe97dc01b7
Dmytro Doroschuk <dimariouz.dev@gmail.com>
2023-02-26 18:00:33 +0200

    added swift ui target

---
+++ WeatherAppCombine/WeatherAppCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppCombineApp.swift
+//  WeatherAppCombine
+//
+//  Created by Dmitry Doroshchuk on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
